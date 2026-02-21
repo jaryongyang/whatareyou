5cc100705202a066b6f751aa6aa031541b32d068
mieieier <mieieier@gmail.com>
2026-02-21 23:00:54 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Add JS here
+const suggestionBtn = document.getElementById("suggestionBtn");
+const foodSuggestion = document.getElementById("foodSuggestion");
+
+const menu = ["치킨", "피자", "햄버거", "라면", "김치찌개", "된장찌개", "초밥", "파스타", "떡볶이"];
+
+suggestionBtn.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * menu.length);
+    foodSuggestion.textContent = menu[randomIndex];
+});
